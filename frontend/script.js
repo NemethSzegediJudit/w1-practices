2dec439469e6f09f6fa57262bb06f59ef38e84c9
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-04-05 20:53:48 +0200

    practices of Thursday

---
+++ frontend/script.js
@@ -0,0 +1,24 @@
+console.log("ez az elso javascriptem");
+/*
+// A stringeket (a szövegeket) idézőjelbe kell rakni.
+"ez itt egy szoveg"
+// A dupla és sima macskaköröm csak egysoros stringet enged meg.
+'ez itt egy masik szoveg'
+    // Backtickek közé rakott stringben lehet sortörés.
+    `ez egy több
+soros szöveg (string)`
+
+//A számokat nem kell idézőjelbe tenni.
+4
+
+128
+
+04
+
+1.13
+*/
+function myFirstFunction() {
+    console.log("Ez az első függvényem belsejében lévő log.")
+}
+
+myFirstFunction()
